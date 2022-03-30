0f6f04ffe843512f306782108760e73cacf6a1e9
alexandermaxwellogle <maxogle@gmail.com>
2022-03-30 17:30:10 -0400

    working thru fetch issues

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ let emailSubmit = document.getElementById('email-submit')
 const pwnRequest = async () => {
     emailSubmit.addEventListener('click', async (e) =>{
         e.preventDefault()
-        let formContent = emailSubmit.innerText
-        let req = await fetch(`https://haveibeenpwned.com/api/v3/breachedaccount/${formContent}hibp-api-key:56821cb5fcf64f1ba1978a81fb61f0c6`)
-        let res = await req.json
+        // let formContent = emailSubmit.innerText
+        let req = await fetch(`https://haveibeenpwned.com/api/v3/breachedaccount/${emailSubmit.value} hibp-api-key:56821cb5fcf64f1ba1978a81fb61f0c6`, {
+            mode: 'no-cors'
+        })
+        let res = await req.json()
         console.log(res)
     })
 }
