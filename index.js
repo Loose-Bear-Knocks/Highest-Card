69c71ec73bc379c723002e8ec94ec8288566f5c3
alexandermaxwellogle <maxogle@gmail.com>
2022-03-30 12:48:55 -0400

    added html and js, begininng fetch functions

---
+++ index.js
@@ -0,0 +1,14 @@
+// have i been pwned API Key : 806e863c28594ad88e5200fd9cfa4cf7
+let emailSubmit = document.getElementById('email-submit')
+
+const pwnRequest = async () => {
+    emailSubmit.addEventListener('click', async (e) =>{
+        e.preventDefault()
+        let formContent = emailSubmit.innerText
+        let req = await fetch(`https://haveibeenpwned.com/api/v3/breachedaccount/${formContent}hibp-api-key:56821cb5fcf64f1ba1978a81fb61f0c6`)
+        let res = await req.json
+        console.log(res)
+    })
+}
+
+pwnRequest()
